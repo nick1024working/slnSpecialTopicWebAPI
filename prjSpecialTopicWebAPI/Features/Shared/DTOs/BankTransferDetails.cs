d1e038b27b380918c74bcf4550f6decc4ede17d5
Simon <namomaahasitatapatreh@outlook.com>
2025-09-03 21:01:52 +0800

    BankTransferDetails

---
+++ prjSpecialTopicWebAPI/Features/Shared/DTOs/BankTransferDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace prjSpecialTopicWebAPI.Features.Shared.DTOs
+{
+    public class BankTransferDetails
+    {
+        public string OrderId { get; set; }
+        public string BankName { get; set; }
+        public string BankCode { get; set; }
+        public string AccountNumber { get; set; }
+        public int Amount { get; set; }
+        public string PaymentDeadline { get; set; }
+    }
+}
